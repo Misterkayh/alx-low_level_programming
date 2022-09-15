687e23d004b01d61166a416be852f65298db4a80
Misterkayh <Misterkayh@gmail.com>
2022-09-15 16:16:39 -0700

    Function checked for capital letter

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,19 +1,16 @@
 #include "main.h"
 
 /**
- * _isdigit - checks for digits
- * @c: parameter
- * Return: 0 or 1
+ * _isupper - Checks for uppercase characters.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if character is uppercase, 0 otherwise.
  */
-
-int _isdigit(int c)
+int _isupper(int c)
 {
-	if (c >= 48 && c < 58)
-	{
+	if (c >= 'A' && c <= 'Z')
 		return (1);
-	}
+
 	else
-	{
 		return (0);
-	}
 }
