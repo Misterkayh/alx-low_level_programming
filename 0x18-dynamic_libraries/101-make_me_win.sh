0d3c9df5c1f829156f7ed2cc88ca860ea35222d7
Misterkayh <misterkayh@gmail.com>
2022-12-20 03:58:57 -0800

    C - Dynamic Libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/misterkayh/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD =/tmp/nrandom.so
+wget -P /tmp/ https://raw.github.com/Misterkayh/alx-low_level_programming/blob/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
